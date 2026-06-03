a422f269023ffc9f70e36655fb226dfd86b93de0
Shivam Prajapati <shivampra5578@gmail.com>
2026-06-03 10:12:05 +0530

    Day-8 uplodaded.

---
+++ Day-8/_31Character_triangle.java
@@ -0,0 +1,14 @@
+public class _31Character_triangle {
+    public static void main(String[] args) {
+        
+        int n=5;
+        for(char i='A';i<='E'; i++){
+            for(char j='A'; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+
+}
