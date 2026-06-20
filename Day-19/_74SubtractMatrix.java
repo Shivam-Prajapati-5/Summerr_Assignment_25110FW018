4dedeb77cc945a3168ad036d1895351ebd895e5d
Shivam Prajapati <shivampra5578@gmail.com>
2026-06-20 16:23:36 +0530

    Day-19 uplodaded.

---
+++ Day-19/_74SubtractMatrix.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class _74SubtractMatrix{
+    public static void main(String[] args) {
+       
+           Scanner sc = new Scanner(System.in);
+        int n = 2;
+        int m = 3;
+        int arr1[][] = new int[n][m];
+        int arr2[][] = new int[n][m];
+        System.out.println("Enter first matrix element :");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                 arr1[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Enter second array element :");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m;j++){
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(arr1[i][j]-arr2[i][j] +" ");
+            }
+            System.out.println("");
+        }
+    }
+
+}
