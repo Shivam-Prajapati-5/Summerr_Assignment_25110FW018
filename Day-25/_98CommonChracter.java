f595f39c4a8dc1de5868374f4c9892cd5a060822
Shivam Prajapati <shivampra5578@gmail.com>
2026-06-20 16:29:08 +0530

    Day-25 uplodaded.

---
+++ Day-25/_98CommonChracter.java
@@ -0,0 +1,16 @@
+public class _98CommonChracter {
+    
+    public static void main(String[] args) {
+
+        String s1 = "hello";
+        String s2 = "world";
+
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+
+            if (s2.indexOf(ch) != -1) {
+                System.out.print(ch + " ");
+            }
+        }
+    }
+}
